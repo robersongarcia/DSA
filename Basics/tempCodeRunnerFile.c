023ab631ce71516ffd3a88258642d18c444e269d
Roberson Garcia <roberson.gar12@gmail.com>
2022-08-18 20:02:41 -0400

    added git ignore and other file

---
+++ Basics/tempCodeRunnerFile.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    int age;
+    printf("Hello World\n");
+    printf("write your age: ");
+    scanf("%i",&age);
+    printf("My age is: %i",age);
+
+    return 0;
+}
